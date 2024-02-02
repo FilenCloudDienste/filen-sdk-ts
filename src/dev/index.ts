165ae3f2ecd246103cc042fc124519c68841d1a1
Dwynr <dwynr@outlook.de>
2024-02-02 02:16:24 +0100

    add: dev dir

---
+++ src/dev/index.ts
@@ -0,0 +1,21 @@
+import FilenSDK from ".."
+import config from "./test.config.json"
+import type { AuthVersion } from "../types"
+
+const filen = new FilenSDK({
+	email: config.email,
+	password: config.password,
+	masterKeys: config.masterKeys,
+	apiKey: config.apiKey,
+	publicKey: config.publicKey,
+	privateKey: config.privateKey,
+	authVersion: config.authVersion as AuthVersion,
+	baseFolderUUID: config.baseFolderUUID,
+	userId: config.userId
+})
+
+const main = async () => {
+	console.log(await filen.api(3).dir().linked().fetch({ uuid: "3c90310b-dc15-47ef-b931-149414aedfe2" }))
+}
+
+main()
