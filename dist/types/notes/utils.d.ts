d41aa7a27e9332c4a276ba82e7cce59b87ee1112
Jan <dwynr@outlook.de>
2025-03-21 02:32:59 +0100

    chore: delete dist

--- dist/types/notes/utils.d.ts
+++
@@ -1,9 +0,0 @@
-import { type NoteType } from "../api/v3/notes";
-export declare function createNotePreviewFromContentText({ content, type }: {
-    content: string;
-    type: NoteType;
-}): string;
-export declare const utils: {
-    createNotePreviewFromContentText: typeof createNotePreviewFromContentText;
-};
-export default utils;
